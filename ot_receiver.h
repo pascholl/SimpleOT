a0142885b18f640d1d4812cf6979b9fbdb8978e3
Peter Scholl <petescholl@gmail.com>
2016-07-28 14:58:56 +0100

    Allow include from C++

--- ot_receiver.h
+++ ot_receiver.h
@@ -1,6 +1,10 @@
 #ifndef OT_RECEIVER_H
 #define OT_RECEIVER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 
 #include "sc25519.h"
@@ -25,6 +29,10 @@ void receiver_maketable(SIMPLEOT_RECEIVER *);
 void receiver_procS(SIMPLEOT_RECEIVER *);
 void receiver_rsgen(SIMPLEOT_RECEIVER *, unsigned char *, unsigned char *);
 void receiver_keygen(SIMPLEOT_RECEIVER *, unsigned char (*)[HASHBYTES]);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //ifndef OT_RECEIVER_H
 
